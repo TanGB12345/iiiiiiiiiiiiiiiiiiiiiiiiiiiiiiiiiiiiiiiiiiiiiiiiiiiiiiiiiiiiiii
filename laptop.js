deb566c47bec2e6343e401186cd0fad60f15d6ee
TanGB12345 <141435070+TanGB12345@users.noreply.github.com>
2023-10-17 18:35:12 +0300

    Add files via upload

---
+++ laptop.js
@@ -0,0 +1,46 @@
+status1 = " ";
+object = [];
+function preload(){
+    image1 = loadImage("laptop.jpg");
+}
+function setup(){
+    canvas = createCanvas(600,400);
+    canvas.center();
+    object = ml5.objectDetector("cocossd",moedloaded);
+ 
+}
+function moedloaded(){
+    console.log("done");
+    status1= true;
+    object.detect(image1,gotresult);
+}
+function gotresult(error,result){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(result);
+        object=result;
+    }
+}
+function draw(){
+    image(image1,0,0,600,400);
+    if(status1 != " "){
+     for(i=0;i<object.length;i++){
+        
+        percent = floor(object[i].confidence*100);
+        fill("red");
+        text(object[i].label + " " +percent+"%",object[i].x,object[i].y);
+        noFill();
+        stroke("red");
+        rect(object[i].x,object[i].y,object[i].width,object[i].height);
+     } 
+    }
+}
+function back(){
+    window.location="index.html";
+
+}
+function bed(){
+    window.location = "bed.html";
+}
---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+function back(){
+    window.location="index.html";
+
+}
+function bed(){
+    window.location = "bed.html";
+}
+function banana(){
+    window.location="banana.html";
+}
+function car(){
+    window.location = "car.html";
+}
+function laptop(){
+    window.location = "laptop.html";
+}
+function people(){
+    window.location = "people.html";
+}
